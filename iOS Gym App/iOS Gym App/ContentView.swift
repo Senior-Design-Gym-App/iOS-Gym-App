6c999e0cd01fbd622ebd4f4ab3277093870af32b
Troy Madden <22maddent@gmail.com>
2025-09-30 12:25:16 -0400

    init

---
+++ iOS Gym App/iOS Gym App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Gym App
+//
+//  Created by Troy Madden on 9/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hi there, I'm a SwiftUI view!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Gym App/iOS Gym App/iOS_Gym_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Gym_AppApp.swift
+//  iOS Gym App
+//
+//  Created by Troy Madden on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Gym_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
